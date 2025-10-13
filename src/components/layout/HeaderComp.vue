67a921f3e25a4260db2aaaa4548cf8c80e9af237
artyom-tugushev <a.tugushev@cartaxi.io>
2025-10-13 17:40:32 +0300

    new components WIP

--- src/components/layout/HeaderComp.vue
+++ src/components/layout/HeaderComp.vue
@@ -1,12 +1,22 @@
 <template>
-  <q-header elevated>
-    <q-btn @click="show"></q-btn>
+  <q-header class="row q-gutter-sm" elevated>
+    <img src="" alt="logo" class="cursor-pointer" @click="router.push('/')" />
+    <q-btn @click="isAddressModalShow = !isAddressModalShow"></q-btn>
   </q-header>
+
+  <AddressModal :visible="isAddressModalShow" />
 </template>
 
 <script setup>
-const show = () => {}
+import { ref } from 'vue'
+const isAddressModalShow = ref(false)
 </script>
 
 <style scoped lang="scss">
+.q-header {
+  height: 5rem;
+  padding: 1rem 1.5rem;
+  border-radius: 0 0 1.5rem 1.5rem;
+  filter: blur(0.4);
+}
 </style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -14,12 +14,6 @@ import HeaderComp from 'src/components/layout/HeaderComp.vue'
 </script>
 
 <style scoped>
-.q-header {
-  height: 5rem;
-  padding: 1rem 1.5rem;
-  border-radius: 0 0 1.5rem 1.5rem;
-  filter: blur(0.4);
-}
 .background {
   background: no-repeat url(../assets/background-baked.jpg);
   background-position: center;
